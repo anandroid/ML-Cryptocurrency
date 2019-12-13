014cfa2d26b585e143038f7beb462de888d06bd6
diadutta <duttadia74@gmail.com>
2019-12-12 20:38:54 -0700

    commit

--- xgboost_local_dia.py
+++ xgboost_local_dia.py
@@ -31,10 +31,10 @@ x_test = test.drop(['PriceUSD', 'date'], axis=1).values.reshape((len(test),4))
 #print(x_train)
 #print(x_test)
 
-xgb = xgb.XGBRegressor(colsample_bytree=0.8, subsample=0.5,
-                               learning_rate=0.001, max_depth=3, min_child_weight=1.8, n_estimators=2000,
-                               reg_alpha=0.1, reg_lambda=0.3, gamma=0.01,
-                               silent=1, random_state =7, nthread = -1)
+xgb = xgb.XGBRegressor(colsample_bytree=1, subsample=0.5,
+                               learning_rate=0.01, max_depth=3, min_child_weight=1.8, n_estimators=10000,
+                               reg_alpha=0.1, reg_lambda=0.01, gamma=0.01,
+                               silent=1, random_state =0, nthread = -1)
 
 
 xgb.fit(x_train, y_train)
@@ -43,15 +43,21 @@ xgb_train = xgb.predict(x_train)
 
 #y_test = xgb.predict(x_train)
 #print(xgb_pred)
-#RMSE = np.sqrt(mean_squared_error(xgb_pred, y_test))
-#print(RMSE.round(4))
+RMSE = np.sqrt(mean_squared_error(xgb_pred, y_test))
+print(RMSE.round(4))
+#print(xgb_pred)
+y_test1 = y_test.flatten();
+#print(y_test)
+print(xgb.score(x_test, y_test1))
+
 
 #date_objects = [datetime.strptime(date, '%m/%d/%Y').date() for date in dates_test]
 #print(dates_test)
 
 plt.figure(0)
-plt.plot(dates_test[-100:], xgb_pred[-100:])
-plt.plot(dates_test[-100:], y_test[-100:])
+plt.plot(range(0, 200), xgb_pred[:200], color="b", label="predicted")
+plt.plot(range(0, 200), y_test1[:200], color="orange", label="actual")
+plt.legend(fontsize=18)
 #plt.figure(1)
 # plt.plot(dates_train, xgb_train)
 # plt.plot(dates_train, y_train)
